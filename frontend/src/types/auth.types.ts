49d846b25d454845f42dc26f1c0f1fe8f93d776c
keletso <keletsomonyamane.dev@gmail.com>
2026-03-08 22:47:56 +0200

    chore: added new files

--- frontend/src/types/auth.types.ts
+++ frontend/src/types/auth.types.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export type UserRole = 'admin' | 'engineer' | 'security_analyst' | 'auditor';
 
 export interface User {
@@ -35,3 +36,42 @@ export interface AuthState {
   isLoading: boolean;
   error: string | null;
 }
+=======
+export type UserRole = 'admin' | 'engineer' | 'security_analyst' | 'auditor';
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: UserRole;
+  createdAt: string;
+}
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+  rememberMe?: boolean;
+}
+
+export interface RegisterPayload {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role: UserRole;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: User;
+}
+
+export interface AuthState {
+  user: User | null;
+  token: string | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  error: string | null;
+}
+>>>>>>> 5a848d3 (chore: added new files)
